ad226209599c02c548a1cb04694b28a95d1ab244
luoxiaojun <luoxiaojun@dodoca.com>
2019-08-19 12:52:16 +0800

    update robot.php

--- robot.php
+++ robot.php
@@ -4,5 +4,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $path = $argv[1];
 
-echo json_encode((new \Lxj\Review\Bot\Bot(require __DIR__ . '/config/analyser.php'))->review($path)->getErrors());
+echo json_encode((new \Lxj\Review\Bot\Bot(require __DIR__ . '/config/analyser.php'))->review($path)->getErrors(), JSON_PRETTY_PRINT);
 echo PHP_EOL;
