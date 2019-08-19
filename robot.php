fbac310492cff601fccbc3f0cc0613c2c2d4cf5b
luoxiaojun <luoxiaojun@dodoca.com>
2019-08-19 23:08:50 +0800

    update robot.php

--- robot.php
+++ robot.php
@@ -2,6 +2,12 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+if (!isset($argv[1])) {
+    echo 'Invalid arguments.';
+    echo PHP_EOL;
+    exit(1);
+}
+
 $path = $argv[1];
 
 echo json_encode((new \Lxj\Review\Bot\Bot(require __DIR__ . '/config/analyser.php'))->review($path)->getErrors(), JSON_PRETTY_PRINT);
